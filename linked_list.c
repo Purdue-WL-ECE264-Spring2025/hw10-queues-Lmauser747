5acf40a5119436bd2e8a45533ac87420e63ca594
Lmauser747 <lucasm2818@gmail.com>
2025-04-04 16:16:10 -0400

    First try?

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,84 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+  struct list_node *new_node = malloc(sizeof(struct list_node));
+  new_node->value = value;
+  new_node->next = NULL;
+  return new_node; 
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *new_head = new_node(value);
+  new_head->next = list->head;
+  list->head = new_head;
+  return;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node *new_tail = new_node(value);
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+  if (list->head == NULL) {
+    list->head = new_tail;
+    return;
+  }
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+  struct list_node *temp = list->head;
+  while (temp->next != NULL) {
+    temp = temp->next;
+  }
 
-void free_list(struct linked_list list) {}
+  temp->next = new_tail;
+  new_tail->next = NULL;
+}
+
+size_t remove_from_head(struct linked_list *list) { 
+  struct list_node *temp = list->head->next;
+  size_t value = list->head->value;
+  free(list->head);
+
+  list->head = temp;
+
+  return value; 
+}
+
+size_t remove_from_tail(struct linked_list *list) { 
+  if(list->head == NULL){
+    return 0;
+  }
+
+  struct list_node *temp = list->head;
+
+  if(temp->next == NULL){
+    size_t value = temp->value;
+    free(temp);
+    list->head = NULL;
+    return value;
+  }
+
+  while(temp->next->next != NULL){
+    temp = temp->next;
+  }
+
+  size_t value = temp->next->value;
+  free(temp->next);
+
+  temp->next = NULL;
+
+  return value; 
+}
+
+void free_list(struct linked_list list) {
+  struct list_node *temp = list.head;
+  struct list_node *temp_2 = temp->next;
+  while(temp->next != NULL){
+    free(temp);
+    temp = temp_2;
+    temp_2 = temp->next;
+  }
+
+  free(temp);
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,99 @@
 #include "queue.h"
+#include "linked_list.h"
 #include "tile_game.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+    size_t new_value = serialize(state);
+    insert_at_tail(&q->data, new_value);
+    return;
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) {  
+    size_t removed_value = remove_from_head(&q->data);
+    return deserialize(removed_value); 
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int check(struct linked_list list, size_t value){
+    struct list_node *temp = list.head;
+    while(temp != NULL){
+        if(temp->value == value){
+            return 1;
+        }
+
+        temp = temp->next;
+    }
+
+    return 0;
+}
+
+int correct_state(struct game_state *state){
+    size_t correct_state = 81985526993846272;
+    uint16_t moves = state->num_steps;
+
+    state->num_steps  = 0;
+
+    if(correct_state == serialize(*state)){
+        state->num_steps = moves;
+        return 1;
+    }else {
+        state->num_steps = moves;
+        return 0;
+    }
+}
+
+int number_of_moves(struct game_state start) { 
+    struct queue q;
+    q.data.head = NULL;
+    struct linked_list visited;
+
+    enqueue(&q, start);
+
+    struct game_state visited_node = start;
+    struct game_state next_state;
+    size_t next_value;
+
+    while(q.data.head != NULL){
+        visited_node = dequeue(&q);
+        
+        if(correct_state(&visited_node)){
+            return visited_node.num_steps;
+        }
+
+        next_state = visited_node;
+
+        move_up(&next_state);
+        next_value = serialize(next_state);
+        if(next_value != serialize(visited_node) && !check(visited, next_value)){
+            enqueue(&q, next_state);
+        }
+        next_state = visited_node;
+
+        move_down(&next_state);
+        next_value = serialize(next_state);
+        if(next_value != serialize(visited_node) && !check(visited, next_value)){
+            enqueue(&q, next_state);
+        }
+        next_state = visited_node;
+
+        move_left(&next_state);
+        next_value = serialize(next_state);
+        if(next_value != serialize(visited_node) && !check(visited, next_value)){
+            enqueue(&q, next_state);
+        }
+        next_state = visited_node;
+
+        move_right(&next_state);
+        next_value = serialize(next_state);
+        if(next_value != serialize(visited_node) && !check(visited, next_value)){
+            enqueue(&q, next_state);
+        }
+        
+        insert_at_head(&visited, serialize(visited_node));
+    
+    }
+
+    return 0; 
+}
